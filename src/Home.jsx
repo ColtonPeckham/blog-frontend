b0f7ca8c0b4465b1b77d6f5f929f937f4b649bb6
Colton Peckham <coltonpeckham@gmail.com>
2022-12-12 14:11:30 -0600

    added dummy data

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,15 @@
+import { PostsIndex } from "./PostsIndex";
+
 export function Home() {
+  const posts = [
+    {id: 1, name: "test1", body: "here is a little body", url: "http://via.placeholder.com/150"},
+    {id: 2, name: "test2", body: "here is a bigger body", url: "http://via.placeholder.com/300"}
+  ]
+  
+  
   return (
     <div>
-      <h1>Welcome to React!</h1>
+      <PostsIndex posts={posts}/>
     </div>
   )
 }
--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -1,7 +1,14 @@
-export function PostsIndex() {
+export function PostsIndex(props) {
   return (
     <div>
-      <h1>All Posts</h1>
+      <h1>Da Blogs</h1>
+      {props.posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.name}</h2>
+          <img src={post.url}/>
+          <body>{post.body}</body>
+          </div>
+        ))}
     </div>
   );
 }
